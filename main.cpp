b8ad872e3358a6541c66c344c67573058a9f313b
Steven Velásquez-Chancí <sjv5461@psu.edu>
2023-08-31 17:07:02 -0400

    Add main program which prints authors first and last name

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main(){
+    auto firstName = std::string("Steven");
+    auto lastName =  std::string("Velasquez Chanci");
+    std::cout << "Hello, My Name is " << firstName << " " << lastName << std::endl;
+    return 0;
+}
